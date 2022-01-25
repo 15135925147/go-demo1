7f887dbd776ee7abbb977905ef230d5729ffb069
ZhaoYapeng <304048177@qq.com>
2022-01-25 15:53:50 +0800

    Emphasize our friendliness

--- say.go
+++ say.go
@@ -1,9 +1,8 @@
 package gomodone
 
-import "fmt" 
+import "fmt"
 
 // say Hi to someone
 func SayHi(name string) string {
-   return fmt.Sprintf("Hi, %s", name)
+	return fmt.Sprintf("Hello,%v\n", name)
 }
-
